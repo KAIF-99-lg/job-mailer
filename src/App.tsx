49cbb92b21b11078c21086d4d6d5b41659c55d1e
KAIF-99-lg <k82102002@gmail.com>
2026-07-30 01:22:29 +0530

    add mail favicon, fix send applications navigation

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ export default function App() {
 
   const renderPage = () => {
     switch (page) {
-      case 'dashboard': return <Dashboard onNavigate={setPage} />
+      case 'dashboard': return <Dashboard onNavigate={handleNavigate} />
       case 'send': return <SendApplication />
       case 'templates': return <Templates />
       case 'history': return <History />
