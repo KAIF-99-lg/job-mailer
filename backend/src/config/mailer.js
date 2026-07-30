17ab5bc325bfd2d801e1cb63a834a4adfa6e46f0
KAIF-99-lg <k82102002@gmail.com>
2026-07-30 23:23:37 +0530

    debug brevo api key

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -3,7 +3,7 @@ const logger = require('../utils/logger');
 const createTransporter = () => ({ apiKey: process.env.BREVO_API_KEY });
 
 const verifyTransporter = async () => {
-  logger.info('Brevo API transporter initialized.');
+  logger.info(`Brevo API transporter initialized. Key present: ${!!process.env.BREVO_API_KEY}`);
 };
 
 module.exports = { createTransporter, verifyTransporter };
